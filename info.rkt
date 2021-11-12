d336c416922550e9fcf50b25db4f7f6baa6bee4d
Stephen De Gabrielle <spdegabrielle@users.noreply.github.com>
2021-11-12 22:45:08 +0000

    Remove deps

--- info.rkt
+++ info.rkt
@@ -1,19 +1,9 @@
 #lang info
 (define collection "qi-quickscripts")
 (define deps '("base"
-               "quickscript"
-               "at-exp-lib"
-               "drracket"
-               "gui-lib"
-               "pict-lib"
-               "racket-index"
-               "scribble-lib"
-               "search-list-box"
-               "srfi-lite-lib"
-               "net-lib"
-               "web-server-lib"))
+               "quickscript"))
 (define build-deps '(#;"scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/qi-quickscripts.scrbl" ())))
 (define pkg-desc "Quickscripts to support the Qi language")
-(define version "0.0")
+(define version "0.1")
 (define pkg-authors '(sdegabrielle))
