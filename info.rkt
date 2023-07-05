662e749cc2edd21d5abb689c28e5052dafc24059
Stephen De Gabrielle <spdegabrielle@users.noreply.github.com>
2023-07-05 10:24:32 +0100

    Update info.rkt add to category  ("DrRacket Quickscripts")

--- info.rkt
+++ info.rkt
@@ -3,7 +3,7 @@
 (define deps '("base"
                "quickscript"))
 (define build-deps '("scribble-lib" "racket-doc"))
-(define scribblings '(("scribblings/qi-quickscripts.scrbl" ())))
+(define scribblings '(("scribblings/qi-quickscripts.scrbl" () ("DrRacket Quickscripts"))))
 (define pkg-desc "Quickscripts to support the Qi language")
 (define version "0.2")
 (define pkg-authors '(sdegabrielle))
